a50e21a08bfe0d55cfb7395da7c98c5618e7081d
BrunoSilva077 <brunosilva1012003@gmail.com>
2023-11-22 21:18:19 +0000

    criaçao do projeto em laravel

---
+++ lab_esof_proj/resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('tittle','Home')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    @include('includes.header')
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
---
+++ lab_esof_proj/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Route::get('/', ['App\Http\Controllers\MainController@home']);
+Route::get('/home', 'App\Http\Controllers\MainController@Home');
+//Route::get('/',[MainController::class, 'home']);
