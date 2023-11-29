1eceb72dc4aeeac14bafa95799ff92742999fdbe
BrunoSilva077 <brunosilva1012003@gmail.com>
2023-11-29 16:26:06 +0000

    responsividade navbar

--- lab_esof_proj/resources/views/includes/header.blade.php
+++ lab_esof_proj/resources/views/includes/header.blade.php
@@ -7,17 +7,17 @@
             </div>
             <div class="grid-item item1 home">
                 <ul class="ul-container">
-                    <li class="btn active"><a href="/home">Home</a></li>
+                    <li class="btn active"><a href="/home" onclick="changeActive(this)">Home</a></li>
                 </ul>
             </div>
             <div class="grid-item item1 products">
                 <ul class="ul-container">
-                    <li class="btn"><a href="#products">Products</a></li>
+                    <li class="btn"><a href="#products" onclick="changeActive(this)">Products</a></li>
                 </ul>
             </div>
             <div class="grid-item item4 contact">
                 <ul class="ul-container">
-                    <li class="btn"><a href="#contacts">Contact us</a></li>
+                    <li class="btn"><a href="#contacts" onclick="changeActive(this)">Contact us</a></li>
                 </ul>
             </div>
             <div class="grid-item item1 search">
@@ -33,7 +33,7 @@
                             <i class="fas fa-shopping-bag fa-2x"></i>
                         </a>
                         <div class="vertical-line"></div>
-                        <a href="" class="menu">
+                        <a class="menu">
                             <i class="fas fa-bars fa-2x"></i>
                         </a>
                     </div>
@@ -50,37 +50,100 @@
                     </a>
                 </div>
             </div>
-            <div class="grid-item item2 responsive-input">
-                    <form>
-                        <input type="text" placeholder="Search.." name="search">
-                    </form>
+            <div class="grid-item item2 responsive-menu-options">
+                <div class="grid-item item2 responsive-input">
+                        <form>
+                            <input type="text" placeholder="Search.." name="search">
+                        </form>
+                </div>
+                <div class="grid-item item2 responsive-menu">
+                    <ul class="ul-container">
+                        <li class="btn active"><a href="/home" onclick="changeActive(this)">Home</a>
+                    </li>
+                    </ul>
+                </div>
+                <div class="grid-item item2 responsive-menu">
+                    <ul class="ul-container">
+                        <li class="btn"><a href="#products" onclick="changeActive(this)">Products</a>
+                    </li>
+                    </ul>
+                </div>
+                <div class="grid-item item2 responsive-menu">
+                    <ul class="ul-container">
+                        <li class="btn">
+                            <a href="#contacts" onclick="changeActive(this)">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="grid-item item2 responsive-menu">
-                <ul class="ul-container">
-                    <li class="btn active"><a href="/home" onclick="changeActive(this)">Home</a></li>
-                </ul>
+            <div class="grid-item item5"></div>
+            <div class="grid-item item1 user-menu-item">
+                <div class="user-menu">
+                    <ul class="ul-container-menu">
+                        <div class="group-li">
+                            <li>
+                                <a href="#login/register">Login/Register</a>
+                                <i class="fas fa-chevron-right"></i>
+                            </li>
+                        </div>
+                        
+                        <hr class="user-menu-hr">
+                        <div class="group-li">
+                            <li>
+                                <a href="#favorites">Favorites</a>
+                                <i class="fas fa-chevron-right"></i>
+                            </li>
+                        </div>
+                        <hr class="user-menu-hr">
+                        <div class="group-li history">
+                            <li>
+                                <a href="#history">Purchase History</a>
+                                <i class="fas fa-chevron-right"></i>
+                            </li>
+                        </div>
+                        <hr class="user-menu-hr">
+                    </ul>
+                </div>
             </div>
-            <div class="grid-item item2 responsive-menu">
-                <ul class="ul-container">
-                    <li class="btn"><a href="#products" onclick="changeActive(this)">Products</a></li>
-                </ul>
-            </div>
-            <div class="grid-item item2 responsive-menu">
-                <ul class="ul-container">
-                    <li class="btn"><a href="#contacts" onclick="changeActive(this)">Contact us</a></li>
-                </ul>
-            </div>
+
+
+            
         </div>
 </nav>
 
 <script>
     document.addEventListener('DOMContentLoaded', function(){
-        const menuBtn = document.querySelector('.grid-item.item1.menu i.fa-bars');
-        const divsResponsiveMenu  = document.querySelectorAll('.grid-item.item2.responsive-menu');
-        const divsResponsiveInput = document.querySelector('.grid-item.item2.responsive-input');
+        const menuBtn = document.querySelector('.menu .menu i');
+        const menuBtnSearchCartContainer = document.querySelector('.search-cart-container .cart-container .menu i');
+        const divsResponsiveMenuOptions = document.querySelector('.responsive-menu-options'); // caixa geral do menu
+        const divsResponsiveMenu  = document.querySelectorAll('.responsive-menu'); // caixa da pesquisa
+        const divsResponsiveInput = document.querySelector('.responsive-input'); // caixa com as opçoes do menu
+        const divHamburguerMenu = document.querySelector('.user-menu-item');
+        const divItem5 = document.querySelector('.grid-item.item5');
 
+        console.log(menuBtnSearchCartContainer);
+
+        menuBtnSearchCartContainer.addEventListener('click',() =>{
+
+            if(divHamburguerMenu.style.display === 'none' || divHamburguerMenu.style.display === ''){
+                divHamburguerMenu.style.display = 'block';
+                divItem5.style.display = 'block';
+            }else{
+                divHamburguerMenu.style.display = 'none';
+                divItem5.style.display = 'none';
+            }
+
+        })
 
         menuBtn.addEventListener('click',function(){
+
+            if(divHamburguerMenu.style.display === 'none' || divHamburguerMenu.style.display === ''){
+                divHamburguerMenu.style.display = 'block';
+                divItem5.style.display = 'block';
+            }else{
+                divHamburguerMenu.style.display = 'none';
+                divItem5.style.display = 'none';
+            }
 
             const windowWidth = window.innerWidth;
 
@@ -93,15 +156,14 @@
                 return windowWidth >= resolution.min && windowWidth <= resolution.max;
             });
 
-
-
-
             divsResponsiveMenu.forEach( function(div) {
 
                 if( (shouldDisplayBlock === true) && (div.style.display === 'none' || div.style.display === '') ){
+                    divsResponsiveMenuOptions.style.display = 'block';
                     divsResponsiveInput.style.display = 'block';
                     div.style.display = 'block';
                 }else{
+                    divsResponsiveMenuOptions.style.display = 'none';
                     divsResponsiveInput.style.display = 'none';
                     div.style.display = 'none';
                 }
