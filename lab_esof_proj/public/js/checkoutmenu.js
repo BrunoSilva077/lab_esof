f637ced8f3f7a2a80246ea40be70157a160792a7
BrunoSilva077 <brunosilva1012003@gmail.com>
2024-01-10 18:42:02 +0000

    update sonar cloud 2

--- lab_esof_proj/public/js/checkoutmenu.js
+++ lab_esof_proj/public/js/checkoutmenu.js
@@ -2,14 +2,14 @@ function mostrarInput() {
     let showmenu = document.getElementById("menuescondido");
     let button = document.getElementById("cntbutton");
 
-           let fname = document.getElementById("fname").value;
-           let lname = document.getElementById("lname").value;
-           let address = document.getElementById("address").value;
-           let pcode = document.getElementById("pcode").value;
-           let VillCity = document.getElementById("VillCity").value;
-           let ContRegion = document.getElementById("ContRegion").value;
-           let email = document.getElementById("email").value;
-           let pnumber = document.getElementById("pnumber").value;
+          //  let fname = document.getElementById("fname").value;
+          //  let lname = document.getElementById("lname").value;
+          //  let address = document.getElementById("address").value;
+          //  let pcode = document.getElementById("pcode").value;
+          //  let VillCity = document.getElementById("VillCity").value;
+          //  let ContRegion = document.getElementById("ContRegion").value;
+          //  let email = document.getElementById("email").value;
+          //  let pnumber = document.getElementById("pnumber").value;
    
            showmenu.style.display = "block"
            button.style.display="none"
