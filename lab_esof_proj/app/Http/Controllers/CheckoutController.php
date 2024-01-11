5ec7d6ec80ecd0c284102290412e516fc275d183
brun4fer <bigbf1130@gmail.com>
2024-01-11 20:33:38 +0000

    guardar checkouts

--- lab_esof_proj/app/Http/Controllers/CheckoutController.php
+++ lab_esof_proj/app/Http/Controllers/CheckoutController.php
@@ -15,62 +15,62 @@ class CheckoutController extends Controller
     {
         //
     }
-    public function session(Request $request)
-{
-    \Stripe\Stripe::setApiKey(config('stripe.sk'));
-
-    $rowIds = $request->input('rowIds');
-    $totals = $request->input('totals');
-    $qtys = $request->input('qtys');
-    $productnames = $request->input('productnames');
-    $voucherCode = $request->input('voucher_code');
-
-    $lineItems = [];
-
-    foreach ($rowIds as $index => $rowId) {
-        $productname = $productnames[$index];
-        $totalprice = $totals[$index];
-        $qty = $qtys[$index];
-        $two0 = "00";
-        $totalAmountInCents = round($totalprice * 100) ;
-
-        $lineItems[] = [
-            'price_data' => [
-                'currency'     => 'EUR',
-                'product_data' => [
-                    "name" => $productname,
+    public function session(Request $request){
+        \Stripe\Stripe::setApiKey(config('stripe.sk'));
+    
+        $rowIds = $request->input('rowIds');
+        $totals = $request->input('totals');
+        $qtys = $request->input('qtys');
+        $productnames = $request->input('productnames');
+        $voucherCode = $request->input('voucher_code');
+    
+        $lineItems = [];
+    
+        foreach ($rowIds as $index => $rowId) {
+            $productname = $productnames[$index];
+            $totalprice = $totals[$index];
+            $qty = $qtys[$index];
+            $two0 = "00";
+            $totalAmountInCents = round($totalprice * 100) ;
+    
+            $lineItems[] = [
+                'price_data' => [
+                    'currency'     => 'EUR',
+                    'product_data' => [
+                        "name" => $productname,
+                    ],
+                    'unit_amount'  => $totalAmountInCents,
                 ],
-                'unit_amount'  => $totalAmountInCents,
+                'quantity'   => $qty,
+            ];
+        }
+    
+        $user = Auth::user();
+    
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => $lineItems,
+            'metadata' => [
+                'voucher_code' => $voucherCode,
             ],
-            'quantity'   => $qty,
-        ];
+            'mode' => 'payment',
+            'success_url' => url('/storecheckout'), //deveria ser store mas nao esta a funcionar por enquanto
+            'cancel_url' => route('checkout', ['user' => auth()->id()]),
+            'customer_email' => $user->email,
+        ]);
+    
+        $request->session()->put('checkout_data', [
+            'rowIds' => $rowIds,
+            'totals' => $totals,
+            'qtys' => $qtys,
+            'productnames' => $productnames,
+            'voucherCode' => $voucherCode,
+            'session_id' => $session->id,
+        ]);
+    
+        return redirect()->away($session->url);
     }
-
-    $user = Auth::user();
-
-    $session = \Stripe\Checkout\Session::create([
-        'payment_method_types' => ['card'],
-        'line_items' => $lineItems,
-        'metadata' => [
-            'voucher_code' => $voucherCode,
-        ],
-        'mode' => 'payment',
-        'success_url' => route('home'), //deveria ser store mas nao esta a funcionar por enquanto
-        'cancel_url' => route('checkout', ['user' => auth()->id()]),
-        'customer_email' => $user->email,
-    ]);
-
-    $request->session()->put('checkout_data', [
-        'rowIds' => $rowIds,
-        'totals' => $totals,
-        'qtys' => $qtys,
-        'productnames' => $productnames,
-        'voucherCode' => $voucherCode,
-        'session_id' => $session->id,
-    ]);
-
-    return redirect()->away($session->url);
-}
+    
     public function success()
     {
         return redirect('home')->with('success', 'Thanks for you order You have just completed your payment. The seeler will reach out to you as soon as possible');
@@ -88,16 +88,31 @@ class CheckoutController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function storecheckout(Request $request)
-    {
-      $payload = $request->getContent();
-      $event = \Stripe\Webhook::constructEvent(
-        $payload, $request->header('Stripe-Signature'), config('cashier.webhook_secret')
-    );
+    public function storecheckout(Request $request){
+        // Recupere os dados da sessão
+        $checkoutData = session('checkout_data');
 
-    $paymentIntentId = $event->data->object->id;
-        return $this->successMethod();
-    }
+        // Se precisar do objeto Request, você pode criá-lo manualmente
+        $request = request();
+
+        // Se precisar acessar dados específicos do Request, faça algo como
+        // $requestData = $request->all();
+
+        // Crie um novo registro na tabela de compras
+        $purchase = new Checkout([
+            'user_id' => Auth::id(),
+            'quantity' => array_sum($checkoutData['qtys']), // Soma das quantidades
+            'total' => array_sum($checkoutData['totals']), // Soma dos totais
+            // Adicione outros campos conforme necessário...
+        ]);
+
+        $purchase->save();
+
+        // Limpe os dados da sessão
+        $request->session()->forget('checkout_data');
+
+        return redirect()->route('home');
+    } 
 
     /**
      * Display the specified resource.
